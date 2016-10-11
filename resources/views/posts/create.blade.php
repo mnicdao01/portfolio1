a11b0c1d29e81ca19eb305b5d9c367d8fd183cf4
mnicdao01 <mnicdao01@yahoo.com>
2016-10-11 14:11:25 +0700

    added Parsley JS Validate

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,12 +6,12 @@
         <div class="col-md-8 col-md-offset-2">
         <h1>Post A New Blog</h1>
         <hr>
-            {!! Form::open(array('route' => 'posts.store')) !!}
+            {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
                 {!! Form::label('title', 'Title:') !!}
-                {!! Form::text('title', null,  array('class' => 'form-control', 'placeholder' => 'Title please..'))  !!}
+                {!! Form::text('title', null,  array('class' => 'form-control', 'placeholder' => 'Title goes here...', 'required' => 'true', 'maxlength' => '255'))  !!}
 
                  {!! Form::label('body', 'Post Body:') !!}
-                 {!! Form::textarea('body', null,  array('class' => 'form-control', 'placeholder' => 'Title please..'))  !!}
+                 {!! Form::textarea('body', null,  array('class' => 'form-control', 'placeholder' => 'Your message here...', 'required' => 'true'))  !!}
 
                  {!! Form::submit('Create New Post', array('class' => 'btn btn-success btn-block'))!!}
             {!! Form::close() !!}
